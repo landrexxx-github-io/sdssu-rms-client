10073f7bbb933e00cd34546879cf55518dd3f5aa
landrexxx <landrex.rebuera@gmail.com>
2021-09-27 09:26:13 +0800

    Updated proposal module

--- src/components/pages/researches/Proposal.jsx
+++ src/components/pages/researches/Proposal.jsx
@@ -138,7 +138,7 @@ const Proposal = ({ currentUser }) => {
             width: proposal.remarks === REMARKS.PENDING ? 80 : null,
             cell: (proposal) => {
                 // Only the Research Head has the access of all the buttons
-                if (proposal.remarks === REMARKS.PENDING && currentUser.user_type === USER_TYPE.USR) {
+                if (proposal.remarks === REMARKS.PENDING && currentUser.user_type === USER_TYPE.ADMIN) {
                     return (
                         <React.Fragment>
                             <Button
